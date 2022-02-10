742c463c4704194606fb062fd50e84983a9342cd
Daisuke Maki <lestrrat+github@gmail.com>
2022-02-10 14:23:19 +0900

    Add grpc example

---
+++ examples/grpc/service_test.go
@@ -0,0 +1,71 @@
+package grpcexample_test
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/lestrrat-go/broadcast"
+	grpcexample "github.com/lestrrat-go/broadcast/examples/grpc"
+	pb "github.com/lestrrat-go/broadcast/examples/grpc/pb"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+func TestService(t *testing.T) {
+	var svc broadcast.Service
+	var ingress grpcexample.Service
+
+	const bufSize = 1024 * 1024
+
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+
+	l := bufconn.Listen(bufSize)
+	go ingress.Run(ctx, &svc, l)
+	go svc.Run(ctx)
+
+	var msgs1 []interface{}
+
+	sub1 := broadcast.SubscribeFunc(func(v interface{}) error {
+		msgs1 = append(msgs1, v)
+		return nil
+	})
+	_ = svc.Subscribe(sub1)
+
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+		return l.DialContext(ctx)
+	}), grpc.WithInsecure())
+	if !assert.NoError(t, err, `grpc.DialContext should succeed`) {
+		return
+	}
+
+	defer conn.Close()
+
+	client := pb.NewBroadcasterClient(conn)
+
+	sendMsgs := []string{`Hello!`, `World`}
+	var expected []interface{}
+
+	for _, payload := range sendMsgs {
+		resp, err := client.Broadcast(ctx, &pb.BroadcastRequest{Message: payload})
+		if !assert.NoError(t, err, `client.Broadcast should succeed`) {
+			return
+		}
+
+		if !assert.True(t, resp.GetSuccess(), `resp.Success should be true`) {
+			return
+		}
+
+		expected = append(expected, payload)
+	}
+
+	time.Sleep(time.Second)
+
+	if !assert.Equal(t, msgs1, expected) {
+		return
+	}
+
+}
